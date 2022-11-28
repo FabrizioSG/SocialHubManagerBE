465f413543b8280b89c24069d9c4acfe904efc51
Carlos Jimenez <carlosjb.cr@gmail.com>
2022-11-28 08:43:30 -0600

    Se desarrollaron los métodos para obtener los posts en cola

--- routes/queues.js
+++ routes/queues.js
@@ -3,6 +3,10 @@ const router = express.Router();
 
 const queueController = require("../controllers/queue.controller");
 
-router.get('/:usuario_id', queueController.getPostsByUserInCola);
+router.get('/posts_in_queue', queueController.getPostsInCola);
+router.get('/posts_in_queue/:usuario_id', queueController.getPostsByUserInCola);
+router.get('/schedules', queueController.getSchedules);
+router.get('/schedules_by_day', queueController.getSchedulesByDay);
+router.get('/schedules_and_posts', queueController.getSchedulesAndPostsByUserAndDayAndTime);
 
 module.exports = router;
